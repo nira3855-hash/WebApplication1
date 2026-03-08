3b6e30e5ae9eb4db3a4df5e6622c5a4d8af0660a
123orchen <orchentr82051@gmail.com>
2026-03-08 19:02:31 +0200

    בדיקת דחיפת שינויים

--- WebApplication/Controllers/UserController.cs
+++ WebApplication/Controllers/UserController.cs
@@ -19,7 +19,6 @@ namespace WebApplication1.Controllers
 
         public UserController(UserIService user)
         {
-            
             this.users = user;
         }
         // GET: CustomerController
