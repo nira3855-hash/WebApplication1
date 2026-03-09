ea476aeb578bf77c194467b9807d2ed436cfbdcb
123orchen <orchentr82051@gmail.com>
2026-03-09 20:48:11 +0200

    add In EventIService

--- Service/Interface/EventIService.cs
+++ Service/Interface/EventIService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Repository.Entities;
+using Service.Dto;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,19 @@ using System.Threading.Tasks;
 
 namespace Service.Interface
 {
-    public class EventIService
+    public interface EventIService
     {
-
+        EventDto AddEvent(EventDto item);
+        void DeleteEvent(int id);
+        List<EventDto> GetAllEvents();
+        EventDto GetEventById(int id);
+        void UpdateEvent(int id, EventDto item);
+        List<Event> GetEventsByProducerId(int producerId);
+        List<Event> GetEventsByDate(DateTime date);
+        List<Event> GetUpcomingEvents();
+        List<Event> SearchEvents(string searchTerm);
+        List<Event> GetEventsByLocation(string location);
+        List<Event> GetEventsByHallId(int hallId);
+        void ValidateEvent(EventDto even);//לא יודעת אם צריך כי בכל מקרה זה private
     }
 }
