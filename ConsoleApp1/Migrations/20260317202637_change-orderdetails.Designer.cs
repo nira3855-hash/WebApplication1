cbe35cd074753840ff688ba1beca5922d92b14c6
TamarDabush <Nira3855@gmail.com>
2026-03-18 02:25:59 +0200

    your message

--- ConsoleApp1/Migrations/20260317202637_change-orderdetails.Designer.cs
+++ ConsoleApp1/Migrations/20260317202637_change-orderdetails.Designer.cs
@@ -4,6 +4,7 @@ using ConsoleApp1.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ConsoleApp1.Migrations
 {
     [DbContext(typeof(TandO))]
-    partial class TandOModelSnapshot : ModelSnapshot
+    [Migration("20260317202637_change-orderdetails")]
+    partial class changeorderdetails
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ConsoleApp1/Migrations/20260317202637_change-orderdetails.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConsoleApp1.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeorderdetails : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Bio",
+                table: "Producer",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetail_HallSeatID",
+                table: "OrderDetail",
+                column: "HallSeatID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderDetail_Event_EventID",
+                table: "OrderDetail",
+                column: "EventID",
+                principalTable: "Event",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderDetail_HallSeat_HallSeatID",
+                table: "OrderDetail",
+                column: "HallSeatID",
+                principalTable: "HallSeat",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderDetail_Event_EventID",
+                table: "OrderDetail");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderDetail_HallSeat_HallSeatID",
+                table: "OrderDetail");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderDetail_HallSeatID",
+                table: "OrderDetail");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Bio",
+                table: "Producer",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
